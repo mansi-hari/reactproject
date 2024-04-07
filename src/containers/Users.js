82281785313aedee6a892b0734fd5b8b9163b13b
mansi-hari <mansigupta1238@gmail.com>
2024-04-07 15:35:10 +0530

    update2

--- src/containers/Users.js
+++ src/containers/Users.js
@@ -1,9 +1,14 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 function Users(){
-    const[users, setUsers] = useState([
-        { name: 'Mohan', age: 25},
-        { name: 'Sohan', age: 26}
-    ])
+    const[users, setUsers] = useState([])
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then (data => {
+            setUsers(data)
+        })
+        
+    },[])
     return(
         <div>
             <h2>
@@ -12,7 +17,7 @@ function Users(){
             <ul>
                 {
                     users.map((user, index)=> {
-                        return <li> {user.name}, {user.age}</li>
+                        return <li> {user.name}, {user.email}</li>
                     })
                 }
             </ul>
